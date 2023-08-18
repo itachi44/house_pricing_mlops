bdbfaa036627dd057c1b9092ed38f7c3003dcd88
itachi44 <diopbamba86@gmail.com>
2023-08-18 14:13:35 +0200

    adding modeling notebook

---
+++ run_notebook.sh
@@ -0,0 +1,16 @@
+#! bin/bash
+
+TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
+EXECUTION_DATE=$(date "+%Y-%m-%d")
+ROOT_DIR=$PWD
+LOG_DIR="${ROOT_DIR}/logs"
+mkdir -p "${LOG_DIR}"
+
+echo "Timestamp: $TIMESTAMP"
+echo "Execution date: $EXECUTION_DATE"
+echo "Root directory: $ROOT_DIR"
+echo "Log directory: $LOG_DIR"
+
+# Execution
+papermill "$ROOT_DIR/notebooks/house_pricing_model_building.ipynb" \
+"$LOG_DIR/${TIMESTAMP}-house_pricing_model_training.ipynb"
